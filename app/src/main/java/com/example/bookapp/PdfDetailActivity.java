0dd71b2961c5907a42f88fe187972cd9b5c76d58
talo33 <114129828+talo33@users.noreply.github.com>
2023-05-28 14:13:41 +0700

    init UI book details

--- app/src/main/java/com/example/bookapp/PdfDetailActivity.java
+++ app/src/main/java/com/example/bookapp/PdfDetailActivity.java
@@ -2,15 +2,32 @@ package com.example.bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.bookapp.databinding.ActivityPdfDetailBinding;
 
 public class PdfDetailActivity extends AppCompatActivity {
 
     //view binding
+    private ActivityPdfDetailBinding binding;
+
+    //pdf id, get from intent
+    String bookId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityPdfDetailBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_pdf_detail);
+
+        //get data from intent e.g. bookId
+        Intent intent = getIntent();
+        bookId = intent.getStringExtra("bookId");
+
+        loadBookDetails();
+    }
+
+    private void loadBookDetails() {
     }
 }
